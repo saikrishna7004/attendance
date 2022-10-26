433b8dcc25bb24fe1442fd98a1c4c0c12f9f76bf
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2022-10-26 16:46:39 +0530

    Added  rel="noreferrer"

--- pages/_app.js
+++ pages/_app.js
@@ -22,13 +22,13 @@ function MyApp({ Component, pageProps }) {
 		<Component {...pageProps} />
 		<footer className="footer-distributed mt-4">
 			<div className="footer-right">
-				<a target="_blank" href="https://www.linkedin.com/in/sai-krishna-karnati/"><FontAwesomeIcon icon={faLinkedinIn} /></a>
-				<a target="_blank" href="https://github.com/saikrishna7004/"><FontAwesomeIcon icon={faGithub} /></a>
-				<a target="_blank" href="https://www.instagram.com/saikrishna7004/"><FontAwesomeIcon icon={faInstagram} /></a>
+				<a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/sai-krishna-karnati/"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+				<a target="_blank" rel="noreferrer" href="https://github.com/saikrishna7004/"><FontAwesomeIcon icon={faGithub} /></a>
+				<a target="_blank" rel="noreferrer" href="https://www.instagram.com/saikrishna7004/"><FontAwesomeIcon icon={faInstagram} /></a>
 			</div>
 			<div className="footer-left">
 				<p className="footer-links">
-					<a className="link-1" target="_blank" href="https://saikrishna.epizy.com/mysites/">My Sites</a>
+					<a className="link-1" target="_blank" rel="noreferrer" href="https://saikrishna.epizy.com/mysites/">My Sites</a>
 				</p>
 				<p>Karnati Sai Krishna &copy; 2022</p>
 			</div>
