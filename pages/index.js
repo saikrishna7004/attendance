cf92067e780e01dff16d7817cf9669d0edfd3d1e
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2023-01-02 12:45:53 +0530

    Added astra error message

--- pages/index.js
+++ pages/index.js
@@ -139,28 +139,6 @@ export default function Home(props) {
 	const [data, setData] = useState(null)
 	const [loading, setLoading] = useState(false)
 	const [attData, setAttData] = useState(null)
-	useEffect(() => {
-		function getCookie(cname) {
-			let name = cname + "=";
-			let decodedCookie = decodeURIComponent(document.cookie);
-			let ca = decodedCookie.split(';');
-			for (let i = 0; i < ca.length; i++) {
-				let c = ca[i];
-				while (c.charAt(0) == ' ') {
-					c = c.substring(1);
-				}
-				if (c.indexOf(name) == 0) {
-					return c.substring(name.length, c.length);
-				}
-			}
-			return "";
-		}
-		let c = getCookie('attendance-rollno')
-		if (c) {
-			setRoll(c.toUpperCase())
-			getAttendance()
-		}
-	}, [])
 
 	const handleInputChange = (e) => {
 		setRoll(e.target.value.toUpperCase().trim())
