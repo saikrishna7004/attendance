d41b59b440324ee2742f597c5b2016e2d2ff77d2
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2023-01-02 20:39:31 +0530

    Added time taken in server

--- pages/api/m_32_314.js
+++ pages/api/m_32_314.js
@@ -18,6 +18,7 @@ export default async function handler(req, res) {
                 console.log(roll)
                 myRoll = roll.netra
             }
+            let start = performance.now()
             var result = await fetch("http://teleuniv.in/netra/api.php", {
                 method: 'POST',
                 headers: {
@@ -39,7 +40,11 @@ export default async function handler(req, res) {
                 })
             }).catch(error => res.status(500).json({ error: 'Internal Server Error', msg: error }))
             var data = await result.json()
+            let end1 = performance.now()
             var data2 = await result2.json()
+            let end2 = performance.now()
+            console.log(start, end1, end2)
+            console.log((end1-start)/1000, (end2-start)/1000)
             return res.status(200).json({m32: data2, m314: data})
         } catch (error) {
             console.log(error);
