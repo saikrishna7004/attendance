a258fd580019a46e2318b4306d3abe08bd59ce8f
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2023-01-02 21:46:17 +0530

    Splitted again int m32 and m314

--- pages/api/m32.js
+++ pages/api/m32.js
@@ -1,19 +1,24 @@
 import connectMongo from '../../utils/connectMongo';
 import Netra from '../../models/netra';
 
+async function connect() {
+    await connectMongo();
+    console.log('CONNECTED TO MONGO');
+}
+connect();
+
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         console.log(req.body)
         if (!req.body.rollno) return res.status(400).json({ error: 'Empty Field Roll No.' })
         try {
-            await connectMongo();
-            console.log('CONNECTED TO MONGO');
             var roll = await Netra.findOne({ rollno: req.body.rollno.toLocaleString().toUpperCase() });
             var myRoll = req.body.rollno
             if (roll) {
                 console.log(roll)
                 myRoll = roll.netra
             }
+            let start = performance.now()
             var result = await fetch("http://teleuniv.in/netra/api.php", {
                 method: 'POST',
                 headers: {
@@ -25,6 +30,8 @@ export default async function handler(req, res) {
                 })
             }).catch(error => res.status(500).json({ error: 'Internal Server Error', msg: error }))
             var data = await result.json()
+            let end = performance.now()
+            console.log("m32", (end-start)/1000)
             return res.status(200).json(data)
         } catch (error) {
             console.log(error);
--- pages/index.js
+++ pages/index.js
@@ -177,8 +177,8 @@ export default function Home(props) {
 		})
 
 		setLoading(true)
-
-		fetch("/api/m_32_314", {
+		
+		fetch("/api/m32", {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json; charset=UTF-8'
@@ -194,7 +194,7 @@ export default function Home(props) {
 				title: 'Oops...',
 				text: 'Something went wrong!',
 			})
-			else if (!data.m32.hallticketno) {
+			else if (!data.hallticketno) {
 				setData(null)
 				setAttData(null)
 				return Swal.fire({
@@ -204,9 +204,37 @@ export default function Home(props) {
 				})
 			}
 			else {
-				console.log(data)
-				setData(data.m32)
-				setAttData(data.m314)
+				// console.log(data)
+				setData(data)
+			}
+		}).catch((e) => {
+			setLoading(false)
+			Swal.fire({
+				icon: 'error',
+				title: 'Oops...',
+				text: 'Something went wrong!',
+			})
+		})
+
+		fetch("/api/m314", {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json; charset=UTF-8'
+			},
+			body: JSON.stringify({
+				// "method": "314",
+				"rollno": roll
+			})
+		}).then((res) => res.json()).then((data) => {
+			setLoading(false)
+			if (data.error || !data.overallattperformance) return Swal.fire({
+				icon: 'error',
+				title: 'Oops...',
+				text: 'Something went wrong!',
+			})
+			else {
+				// console.log(data)
+				setAttData(data)
 			}
 		}).catch((e) => {
 			setLoading(false)
